9cfea908f12ae4526c15cec974d7f7ffed529d79
Nishant S <nishantns7@gmail.com>
2018-01-24 19:36:30 +0530

    Week 1 Problem 3

--- alphaSub.py
+++ alphaSub.py
@@ -1,21 +1,22 @@
 s='azcbobobegghakl'
 
 i=0
-l=len(s)
+length=len(s)
 max=0
-while(i<l-1):
+while(i<length-1):
     c=0
     j=i
-    while(s[i]<=s[i+1] and i<l-1):
+    while(s[i]<=s[i+1]):
         c+=1
-        print(i)
         i+=1
+        if(i==length-1):
+            break
     if(c>max):
         max=c
-        l=j
-        u=i
+        lower=j
+        upper=i
     i+=1
 sub=''
-for i in range(l,u+1):
-    sub+=s[i]
+for k in range(lower,upper+1):
+    sub+=s[k]
 print("Longest substring in alphabetical order is: "+sub)
